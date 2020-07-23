80d874d1f392c55476d5159b4bd6b8e96f98d854
tung <tung.nguyen271193@gmail.com>
2020-07-23 20:36:02 +0300

    connect to database

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const postgres = knex ({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'tung',
+    password : '',
+    database : 'face-recognition-brain'
+  }
+});
+
+console.log(postgres.select('*').from('users'));
 
 const app = express();
 
