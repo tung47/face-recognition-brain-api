76b8cf9d49a3e4f8687d5cf8d046cb52a09ea62b
tung <tung.nguyen271193@gmail.com>
2020-07-24 13:42:29 +0300

    remove database object

--- server.js
+++ server.js
@@ -19,34 +19,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      password: 'cookies',
-      email: 'john@gmail.com',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      password: 'bananas',
-      email: 'sally@gmail.com',
-      entries: 0,
-      joined: new Date(),
-    }    
-  ],
-  login: [
-    {
-      id: '987',
-      hash: '',
-      email: 'john@gmail.com'
-    }
-  ]
-}
-
 app.get('/', (req, res) => {
   res.send(database.users);
 })
