b97f9830d675c55abb64a86ec35f7f4d18a83b99
tung <tung.nguyen271193@gmail.com>
2020-07-24 17:30:26 +0300

    modify

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ const db = knex({
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
-app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send(database.users);
-})
+// app.get('/', (req, res)=> {
+//   res.send(database.users);
+// })
 
 app.post('/signin', (req, res) => {
   db.select('email', 'hash').from('login')
@@ -45,40 +45,39 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
   const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
     })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
-        .returning('*')
-        .insert({ 
-          email: loginEmail[0], 
-          name: name, 
-          joined: new Date()
-        })
-        .then(user => {
-          res.json(user[0]);
-        })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-  
     .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  db.select('*').from('users').where({id}) 
+  db.select('*').from('users').where({id})
     .then(user => {
       if (user.length) {
         res.json(user[0])
       } else {
-        res.status(400).json('not found')
+        res.status(400).json('Not found')
       }
     })
     .catch(err => res.status(400).json('error getting user'))
@@ -95,6 +94,6 @@ app.put('/image', (req, res) => {
   .catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.listen(3000, () => {
-  console.log('App is running on port 3000');
-});
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
