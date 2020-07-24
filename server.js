ca0d16522b3653c4fcc3ea5716d438b6c57a10a9
tung <tung.nguyen271193@gmail.com>
2020-07-24 11:51:17 +0300

    update entries

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  db.select('*').from('users').where({id: id}) 
+  db.select('*').from('users').where({id}) 
     .then(user => {
       if (user.length) {
         res.json(user[0])
@@ -94,17 +94,13 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++
-      return res.json(user.entries);
-    } 
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
   })
-  if (!found) {
-    res.status(400).json('Not found');
-  }  
+  .catch(err => res.status(400).json('Unable to get entries'))
 })
 
 app.listen(3000, () => {
