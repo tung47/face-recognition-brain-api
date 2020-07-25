bb1bc5813f2eacbc9052afae6c6753755771f585
tung <tung.nguyen271193@gmail.com>
2020-07-25 20:12:24 +0300

    database connect

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-transparent-04763',
-    user : 'tung',
-    password : 'qwe123',
-    database : 'face-recognition-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
