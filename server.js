b4cb74403fec8ddf0a376ccb3f5ceb7842cf1552
tung <tung.nguyen271193@gmail.com>
2020-07-25 11:15:58 +0300

    add profile.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
 
 const db = knex({
   client: 'pg',
@@ -30,16 +31,7 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
