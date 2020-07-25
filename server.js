8e4d8b4d2040f8f4ad78bfcd7599c01c0714e783
tung <tung.nguyen271193@gmail.com>
2020-07-25 20:06:17 +0300

    add heroku database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-transparent-04763',
     user : 'tung',
     password : 'qwe123',
     database : 'face-recognition-brain'
