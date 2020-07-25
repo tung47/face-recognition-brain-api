4dcedbc2f16dabb7cb59592f062db33e9e62be31
tung <tung.nguyen271193@gmail.com>
2020-07-25 12:17:58 +0300

    move clarifai api to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+  apiKey: '65714616bdb64b4cba8ccf90cd893861'
+});
+
+const handleApiCall = (req, res) => { 
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) =>  {
   const { id } = req.body;
   db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) =>  {
 }
 
 module.exports = {
-  handleImage
+  handleImage, 
+  handleApiCall
 }
