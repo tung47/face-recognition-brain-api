bbb1ce5893327d9fa7cc8e75cf65869ee9071aaf
tung <tung.nguyen271193@gmail.com>
2020-07-25 11:21:34 +0300

    add image.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
@@ -26,11 +27,8 @@ app.use(bodyParser.json());
 app.get('/', (req, res)=> { res.send(db.users) })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
-
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
-
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 app.listen(3000, ()=> {
